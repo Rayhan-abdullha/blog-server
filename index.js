db13212ef8721864ce9ee6e921686d9c44fef8b3
Rayhan-abdullha <captainrayhan215@gmail.com>
2023-09-28 10:39:13 +0600

    changes

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
 const http = require("http");
-const server = http.createServer(app);
 
 const port = process.env.PORT || 5000;
 
 async function connectionDB() {
   try {
-    await mongoose.connect(process.env.DB_URL);
-    server.listen(port, () => {
+    mongoose.connect(process.env.DB_URL);
+    app.listen(port, () => {
       console.log(`http://localhost:${port}`);
     });
   } catch (e) {
